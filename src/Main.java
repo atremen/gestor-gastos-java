af3c72a5397bc202857fa425ed78753279bb979c
ddavalos01 <ddavalos01@alumnos.uaq.mx>
2026-08-24 23:26:51 -0600

    Agregar funciones de mostrarGastoMayor, consultarGastoPorCategoria y mostrarMenu.

--- src/Main.java
+++ src/Main.java
@@ -24,62 +24,21 @@ public class Main {
 
     public static double calcularTotalPorCategoria(ArrayList<String> categorias,
                                                    ArrayList<Double> montos,
-                                                   String categoriaBuscada,
-                                                   ArrayList<Double> montosAlimentos,
-                                                   ArrayList<Double> montosTransporte,
-                                                   ArrayList<Double> montosMaterialesEscolares,
-                                                   ArrayList<Double> montosEntretenimiento,
-                                                   ArrayList<Double> montosOtros,
-                                                   Scanner scanner){
-        boolean categoriaValida = false;
-        String categoria = "";
-        while (!categoriaValida) {
-            out.println("Categorías disponibles: Alimentos, Transporte, Materiales Escolares, Entretenimiento, Otros");
-            out.print("Ingrese la categoría: ");
-            categoria = scanner.nextLine();
-
-            if (categoria.equalsIgnoreCase("Alimentos") ||
-                    categoria.equalsIgnoreCase("Transporte") ||
-                    categoria.equalsIgnoreCase("Materiales Escolares") ||
-                    categoria.equalsIgnoreCase("Entretenimiento") ||
-                    categoria.equalsIgnoreCase("Otros")) {
-                categoriaValida = true;
-            } else {
-                out.println("Categoría no válida. Intente de nuevo.");
-            }
-        }
-
-        int numCategoria;
-        float total = 0;
-        if(categoriaBuscada.equalsIgnoreCase("Alimentos"){
-            for(Double item:montosAlimentos){
-                total += item;
-            }
-        }
-        if(categoriaBuscada.equalsIgnoreCase("Transporte"){
-            for(Double item:montosTransporte){
-                total += item;
-            }
-        }
-        if(categoriaBuscada.equalsIgnoreCase("Materiales Escolares"){
-            for(Double item:montosMaterialesEscolares){
-                total += item;
-            }
-        }
-        if(categoriaBuscada.equalsIgnoreCase("Entretenimiento"){
-            for(Double item:montosEntretenimiento){
-                total += item;
-            }
-        }
-        if(categoriaBuscada.equalsIgnoreCase("Otros")){
-            for(Double item:montosOtros){
-                total += item;
+                                                   String categoriaBuscada){
+        double total = 0.0;
+        for(int i = 0; i < categorias.size(); i++){
+            if( categorias.get(i).equalsIgnoreCase(categoriaBuscada )){
+                total += montos.get(i);
             }
         }
         return total;
     }
 
-    public static void mostrarResumen(ArrayList<String> conceptos, ArrayList<String> categorias, ArrayList<Double> montos, float total, int posicionGastoMayor){
+    public static void mostrarResumen(ArrayList<String>conceptos,
+                                      ArrayList<String> categorias,
+                                      ArrayList<Double> montos,
+                                      float total,
+                                      int posicionGastoMayor){
         float promedio = total / montos.size();
         out.print("\nRESUMEN SEMANAL\n");
         out.printf("\nNumero de gastos: %d", montos.size() );
@@ -88,14 +47,88 @@ public class Main {
         out.printf("\nGasto mayor: %s, %.2f", conceptos.get(posicionGastoMayor), montos.get(posicionGastoMayor) );
     }
 
+    public static void mostrarMenu(){
+        System.out.println("1) Registrar nuevo gasto");
+        System.out.println("2) Mostrar todos los gastos");
+        System.out.println("3) Mostrar total de gastos");
+        System.out.println("4) Mostrar gasto mayor");
+        System.out.println("5) Consultar gasto por categoria");
+        System.out.println("6) Mostrar resumen");
+        System.out.println("7) Salir");
+    }
+
+    public static void mostrarGastoMayor(ArrayList<String> conceptos,
+                                         ArrayList<String> categorias,
+                                         ArrayList<Double> montos){
+        int posicion = obtenerPosicionGastoMayor(montos);
+        System.out.println("Gasto mayor:");
+        System.out.println("Concepto: " + conceptos.get(posicion));
+        System.out.println("Categoria: " + categorias.get(posicion));
+        System.out.println("Monto: $" + montos.get(posicion));
+    }
+
+    public static void consultarGastosPorCategoria(ArrayList<String> categorias,
+                                                   ArrayList<Double> montos,
+                                                   Scanner scanner){
+        System.out.print("Ingresar categoria a consultar: ");
+        String categoria = scanner.nextLine();
+        Double total = calcularTotalPorCategoria(categorias, montos, categoria);
+        System.out.println("Gasto total de " + categoria + ": $" + total);
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Gestor semanal de gastos");
 
         ArrayList<String> conceptos = new ArrayList<>();
-        String[] categorias = new String[5];
-        double[] montos = new double[5];
+        ArrayList<String> categorias = new ArrayList<>();
+        ArrayList<Double> montos = new ArrayList<>();
 
+        int opcion;
+
+        do {
+
+            mostrarMenu();
+
+            opcion = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcion) {
+
+                case 1:
+                    registrarGasto(conceptos, categorias, montos, scanner);
+                    break;
+
+                case 2:
+                    mostrarGastos(conceptos, categorias, montos);
+                    break;
+
+                case 3:
+                    System.out.println("Total: $" + calcularTotal(montos));
+                    break;
+
+                case 4:
+                    mostrarGastoMayor(conceptos, categorias, montos);
+                    break;
+
+                case 5:
+                    consultarGastosPorCategoria(categorias, montos, scanner);
+                    break;
+
+                case 6:
+                    mostrarResumen(conceptos, categorias, montos);
+                    break;
+
+                case 7:
+                    System.out.println("Programa terminado.");
+                    break;
+
+                default:
+                    System.out.println("Opción no válida.");
+
+            }
+        } while (opcion != 7);
+        scanner.close();
     }
 
     public static void mostrarGastos(
